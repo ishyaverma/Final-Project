c446bcc6d5a121fa1d154dac06b66735583e7ebe
ishyaverma <ishyaverma98@gmail.com>
2015-08-11 09:36:28 -0400

    Final

---
+++ final.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function fadeOut(){
+    $(".btn1").click(function(){
+        $("p").fadeOut()
+    });
+    $(".btn2").click(function fadeIn(){
+        $("p").fadeIn(); 
+    });
+}); 
+
+
+	fadeIn ();
+	fadeOut();
+
+
